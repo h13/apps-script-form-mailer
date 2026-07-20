9469a26084d4eac24c2444a66d89243cf61ef33d
Hironori Takahashi <h13.webmail@gmail.com>
2026-07-20 21:45:57 +0900

    chore: upgrade TypeScript to v7 (#11)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import swc from '@rollup/plugin-swc';
 import cleanup from 'rollup-plugin-cleanup';
-import typescript from 'rollup-plugin-typescript2';
 
 export default {
   input: 'src/index.ts',
@@ -7,6 +8,10 @@ export default {
     dir: 'dist',
     format: 'esm',
   },
-  plugins: [cleanup({ comments: 'none', extensions: ['.ts'] }), typescript()],
+  plugins: [
+    nodeResolve({ extensions: ['.js', '.ts'] }),
+    cleanup({ comments: 'none', extensions: ['.ts'] }),
+    swc(),
+  ],
   context: 'this',
 };
